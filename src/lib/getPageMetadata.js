093322a4d607804612ce969722c08638de0b8f37
Batuhan Y. Yılmaz <70820809+B4tuhanY1lmaz@users.noreply.github.com>
2024-05-06 13:27:16 +0300

    Add api route for modal content

--- src/lib/getPageMetadata.js
+++ src/lib/getPageMetadata.js
@@ -2,6 +2,30 @@ import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 
+function getModalContent(modal) {
+    const dir = path.resolve("./config", "modals")
+    const file = `${dir}/${modal}.md`
+    const content = fs.readFileSync(file, "utf8")
+
+    return matter(content).content
+}
+
+function getAllPages() {
+    const dir = path.resolve('./config', 'pages')
+    const files = fs.readdirSync(dir)
+    const mPages = files.filter((file) => file.endsWith(".md"))
+
+    mPages.forEach((file) => {
+        const content = fs.readFileSync(`${dir}/${file}`, "utf-8")
+        const result = matter(content)
+
+        return {
+            slug: file.replace(".md", ""),
+            content: result.content
+        }
+    })
+}
+
 function getPageContent(page) {
     // Read page files over ".config/pages/x.md"
     const dir = path.resolve('./config', 'pages')
@@ -11,4 +35,4 @@ function getPageContent(page) {
     return matter(content)
 }
 
-export { getPageContent }
+export { getPageContent, getAllPages, getModalContent }
