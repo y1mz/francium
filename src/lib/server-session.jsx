4bf243a16d3e85e399ef332a3cd83a3f0cbd6b24
Batuhan Y. Yılmaz <batuhan@yyilmaz.com.tr>
2024-05-06 19:29:22 +0300

    Delete server-session.jsx

--- src/lib/server-session.jsx
+++
@@ -1,8 +0,0 @@
-import { getServerSession } from "next-auth"
-import { authOptions } from "@/app/(auth)/api/[...nextauth]/route"
-
-async function ServerSession() {
-    return await getServerSession(authOptions)
-}
-
-export { ServerSession }
